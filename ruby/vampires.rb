163e10e8f2c83f26463066d1e85ba08062a7da3a
lizk0189 <lizk0189@gmail.com>
2017-01-29 17:05:58 -0600

    Complete release 3

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,29 +1,36 @@
 # Gather data on new employee
 
-puts "What is your name?"
-  name = gets.chomp
+puts "How many employees will be processed?"
+  employees = gets.chomp.to_i
 
-puts "How old are you?"
-  age = gets.chomp.to_i
+number_of_employees = 0
+while number_of_employees < employees
+  number_of_employees +=1
 
-puts "What year were you born?"
-  birth_year = gets.chomp.to_i
+  puts "What is your name?"
+    name = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you (yes or no)?"
-  garlic_bread = gets.chomp
+  puts "How old are you?"
+    age = gets.chomp.to_i
 
-puts "Would you like to enroll in the company’s health insurance (yes or no)?"
-   health_insurance = gets.chomp
+  puts "What year were you born?"
+    birth_year = gets.chomp.to_i
 
-if (name != "Drake Cula" && "Tu Fang") && (age == (2017 - birth_year)) && garlic_bread == "yes" && health_insurance == "yes"
-puts "Probably not a vampire."
-elsif (name != "Drake Cula" && "Tu Fang") && (age != 2017 - birth_year) && (garlic_bread == "no" || health_insurance == "no")
-  puts "Probably a vampire."
-elsif (name != "Drake Cula" && "Tu Fang") && (age != 2017 - birth_year) && garlic_bread == "no" && health_insurance == "no"
-  puts "Almost certainly a vampire."
-elsif name == "Drake Cula" || "Tu Fang"
-  puts "Definitely a vampire."
-else
-  puts "Results inconclusive."
+  puts "Our company cafeteria serves garlic bread. Should we order some for you (yes or no)?"
+    garlic_bread = gets.chomp
+
+  puts "Would you like to enroll in the company’s health insurance (yes or no)?"
+     health_insurance = gets.chomp
+
+  if (name != "Drake Cula" && "Tu Fang") && (age == (2017 - birth_year)) && garlic_bread == "yes" && health_insurance == "yes"
+  puts "Probably not a vampire."
+  elsif (name != "Drake Cula" && "Tu Fang") && (age != 2017 - birth_year) && (garlic_bread == "no" || health_insurance == "no")
+    puts "Probably a vampire."
+  elsif (name != "Drake Cula" && "Tu Fang") && (age != 2017 - birth_year) && garlic_bread == "no" && health_insurance == "no"
+    puts "Almost certainly a vampire."
+  elsif name == "Drake Cula" || "Tu Fang"
+    puts "Definitely a vampire."
+  else
+    puts "Results inconclusive."
+  end
 end
-
