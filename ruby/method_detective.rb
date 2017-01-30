3d30171cb7a0e6d3322eea41b1b2c405cf219a92
Liz Kaczmarski <lizk0189@gmail.com>
2017-01-30 15:13:51 -0600

    Update method_detective.rb

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,22 +4,22 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+# "iNvEsTiGaTiOn".<swapcase>
 # => “InVeStIgAtIoN”
 
 # "zom".<???>
 # => “zoom”
 
-# "enhance".<???>
+# "enhance".<center>
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+# "Stop! You’re under arrest!".<upcase>
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+# "the usual".<concat>
 #=> "the usual suspects"
 
-# " suspects".<???>
+# " suspects".<concat>
 # => "the usual suspects"
 
 # "The case of the disappearing last letter".<???>
@@ -28,12 +28,12 @@
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# "Elementary,    my   dear        Watson!".<strip>
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+# "z".<ord>
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# "How many times does the letter 'a' appear in this string?".<count(a)>
 # => 4
