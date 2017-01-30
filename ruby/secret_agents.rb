41fb7844f5490a756200ef1a490ddb3eac6c2d34
lizk0189 <lizk0189@gmail.com>
2017-01-29 20:34:45 -0600

    Initial commit

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,5 @@
+=begin
+- Create and define the methods encrypt and decrypt (def)
+- Create an encrypt method that advances every letter of a string one letter forward
+- Decrypt method that reverses the encrypt method (moves every letter one space back). Find where the leter is and access the letter before it.
+=end
