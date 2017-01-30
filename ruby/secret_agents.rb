4a2f7c980b1f818a9b31acbcce3a05fbe525c881
lizk0189 <lizk0189@gmail.com>
2017-01-29 20:48:11 -0600

    Complete release 5

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -11,7 +11,7 @@ def encrypt(password1)
   while index < password1.length
     for x in 0...password1.length
       if password1[x] == "z"
-        password1[x] == "a"
+        password1[x] = "a"
       else
         password1[index] = password1[index].next
       end
@@ -29,7 +29,7 @@ def decrypt(password2)
   while index < password2.length
     for x in 0...password2.length
       if password2[x] == "a"
-        password2[x] == "z"
+        password2[x] = "z"
       else
         result = alpha[(alpha.index(password2[index]) - 1)]
         password2[index] = result
@@ -39,5 +39,25 @@ def decrypt(password2)
   end
   password2
 end
+
+# OUR DRIVER CODE
+
+# Asks a secret agent (the user) whether they would like to decrypt or encrypt a password
+
+puts "Do you want decrypt or encrypt a password?"
+answer = gets.chomp
+
+# Asks them for the password
+
+puts "What is the password you would like to use?"
+password = gets.chomp
+
+# Conducts the requested operation, prints the result to the screen, and exits
+
+  if answer == "encrypt"
+    puts encrypt(password)
+  else
+    puts decrypt(password)
+  end
 
 #decrypt(encrypt("swordfish")) will output "swordfish"; Encrypt advances the letters forward one space and decrypt pushes it back to the original string.
