02d9ba6be6293ec77bdba40f61a61d7efe08e814
lizk0189 <lizk0189@gmail.com>
2017-01-31 10:57:20 -0600

    Complete release 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,38 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<swapcase>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<tr_s('o', 'oo'>
+"zom".gsub('o', 'oo')
 # => “zoom”
 
-# "enhance".<center(20)>
+"enhance".center(20)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<upcase>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<concat>
+"the usual".concat
 #=> "the usual suspects"
 
-# " suspects".<concat>
+" suspects".concat
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<chop>
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<slice(0)>
+
+"The mystery of the missing first letter".slice(0)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<strip>
+"Elementary,    my   dear        Watson!".gsub(/\s+/, " ") 
 # => "Elementary, my dear Watson!"
 
-# "z".<ord>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+ #Number 122 is what it correpsonds to in the ASCII code.
 
-# "How many times does the letter 'a' appear in this string?".<count(a)>
+"How many times does the letter 'a' appear in this string?".count"a"
 # => 4
