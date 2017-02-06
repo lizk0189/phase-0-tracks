52887aef830655a44f6501c3b32aff91da63b147
lizk0189 <lizk0189@gmail.com>
2017-02-05 18:40:12 -0600

    Add key

---
+++ ruby/hashes.rb
@@ -0,0 +1,24 @@
+
+## 1. Build a hash
+application = {
+
+## 2. Ask the user for the client's name, age, number of 
+## children, decor theme, number of rooms, 
+## pay in advance (true/false), picky (true/false). estimated
+## project length
+	client_name:
+	age:
+	number_of_children:
+	decor_theme:
+	number_of_rooms:
+	pay_in_advance:
+	picky: 
+	project_length:
+}
+## 3. Convert user input to apropriate date type (string, integer)
+## 4. Print the hash on the screen when all questions are answered
+p application
+## 5. Allow user to update a key. IF the designer says no, skip it.
+## 6. Print the latest version of the hash and exit.
+
+
