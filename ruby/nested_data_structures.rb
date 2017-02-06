e475468500b85bc8d6bb2921a6645b64c5d93659
lizk0189 <lizk0189@gmail.com>
2017-02-05 20:15:05 -0600

    Create nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,27 @@
+restaurant = {
+	bar: {
+		formal_name: 'Bar Area',
+		number_of_servers: {
+			male: 6,
+			female: 4
+		},
+		specials: ["red wine", "vodka soda"
+		]
+	},
+	cafe: {
+		formal_name: 'Cafe Area',
+		number_of_servers: {
+			male: 10,
+			female: 8
+		},
+		specials: ["brussels sprouts", "pasta"]
+	},
+	main: {
+		formal_name: 'Main Dining Room',
+		number_of_servers: {
+			male: 2,
+			female: 11
+		},
+		specials: ["meatloaf", "salad"]
+	}
+}
