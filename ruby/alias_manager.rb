3d12d3c8d763f352ee752e9513b99e5715f26346
lizk0189 <lizk0189@gmail.com>
2017-02-06 08:24:30 -0600

    Complete challenge

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -9,7 +9,21 @@
 5. Create empty hash to store user names.
 =end
 
-def convert_letters(input)
+
+# Swap first and last name
+=begin
+puts "What is your first name?"
+	first_name = gets.chomp
+puts "What is your last name?"
+	last_name = gets.chomp
+
+new_name = last_name + " " + first_name
+puts new_name
+=end 
+
+# Change vowels and consonants to proceeding vowels/consonants.
+# Account for edge cases
+def next_letter(input)
 	vowels = "aeiou"
 	consonants = "bcdfghjklmnpqrstvwxyz"
 	if vowels.include?(input)
@@ -29,3 +43,36 @@ def convert_letters(input)
 	end
 end
 
+def switch_name(input)
+	real_name = " "
+	reverse_name = input.split (" ").reverse.join(" ")
+	reverse_name.chars.each do |letter|
+		real_name += next_letter(letter)
+	end
+	real_name.split.map!{|letter| letter}.join(" ")
+end
+
+
+# Create hash to store all name entries
+
+all_names = {}
+
+loop do	
+	until name == 'quit'
+	puts "Please enter your name (or quit to exit)."
+	name = gets.chomp.downcase
+	fake_name = switch_name(name)
+	puts "Your fake name is: #{fake_name}."
+	if name == 'quit'
+		break
+	else
+		all_names[name] = fake_name
+	end
+end
+
+all_names.each do |name, fake_name| 
+	puts "#{fake_name} is actually code for #{name}."
+end 
+
+
+	
