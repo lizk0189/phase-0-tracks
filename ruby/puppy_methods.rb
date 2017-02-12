ce30c7266f248565351101cf1c2e56f53a3c41c3
lizk0189 <lizk0189@gmail.com>
2017-02-12 17:49:17 -0600

    Create methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+	def speak(n)
+		puts "Woof!" * n
+	end
+
+	def roll_over
+		puts "*rolls over*"
+	end
+
+	def dog_years(human_years)
+		puts human_years * 7
+	end
+	def bark(message)
+		puts message.upcase
+	end
 end
 
+#DRIVER CODE
+
+puppy = Puppy.new
+puppy.fetch('frisbee')
+puppy.speak(4)
+puppy.roll_over
+puppy.dog_years(4)
+puppy.bark('hello')
