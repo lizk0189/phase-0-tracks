32f19e3e9cb18ca4b7b12ccfa05e7d1736c3f498
lizk0189 <lizk0189@gmail.com>
2017-02-12 19:12:46 -0600

    Complete release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,5 @@
 class Santa
+	DEFAULT_QUANTITY = 0
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
@@ -6,12 +7,32 @@ class Santa
 		puts "That was a good #{cookie} cookie!" 
 		cookie
 	end
-	def initialize
+	def initialize(gender,ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+	end
+	def age(quantity=DEFAULT_QUANTITY)
 	end
 end
 
+reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 
-santa = Santa.new
+#Driver Code
+=begin
+santa = Santa.new("female","Asian")
 santa.speak
-santa.eat_milk_and_cookies('oatmeal')
+santa.eat_milk_and_cookies(oatmeal)
+=end
+
+=begin 
+santas = []
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+10.times do
+	santas << Santa.new(genders.sample, ethnicities.sample)
+end
+santas.each do
+	puts "Santa identifies as #{@gender} and #{@ethnicity}."
+end
+=end
