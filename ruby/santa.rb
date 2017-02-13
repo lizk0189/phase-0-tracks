b61532f36d0db5601b25aba1bf85d310c0c5d4af
lizk0189 <lizk0189@gmail.com>
2017-02-12 19:39:23 -0600

    Complete Release 3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,29 +1,45 @@
 class Santa
-	DEFAULT_QUANTITY = 0
+		attr_reader :age, :ethnicity
+		attr_accessor :gender
+
+		def initialize(gender,ethnicity)
+		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
+
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie} cookie!" 
 		cookie
 	end
-	def initialize(gender,ethnicity)
-		puts "Initializing Santa instance ..."
-		@gender = gender
-		@ethnicity = ethnicity
+
+	def celebrate_birthday
+		@age +=1
+		puts "Your age is #{@age}"
 	end
-	def age(quantity=DEFAULT_QUANTITY)
+
+	def get_mad_at(bad_reindeer)
+		@reindeer_ranking.delete(bad_reindeer)
+		@reindeer_ranking << bad_reindeer
+		puts "I am mad at you #{bad_reindeer}."
+		puts @reindeer_ranking
 	end
 end
 
-reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+##Driver Code##
 
-#Driver Code
-=begin
-santa = Santa.new("female","Asian")
+santa = Santa.new("female","Ethiopian")
 santa.speak
-santa.eat_milk_and_cookies(oatmeal)
-=end
+santa.eat_milk_and_cookies("oatmeal")
+santa.celebrate_birthday
+santa.get_mad_at("Rudolph")
+santa.gender = "idk"
 
 =begin 
 santas = []
